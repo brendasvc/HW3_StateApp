68c3cf9d86028d528d24eb1b234bd4a8778e13e0
Brenda Vega Contreras <bvega96@outlook.com>
2024-05-05 14:04:16 -0700

    Setting up pages

---
+++ UI/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, Button, StyleSheet, ImageBackground, TouchableOpacity, FlatList } from 'react-native';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native'; // Import useFocusEffect
+
+import styles from "./CommonStyleSheet";
+//import MyImage from '../assets/bgimages/Picture1.png';
+
+export default function Dashboard({ navigation }) {
+
+
+}
